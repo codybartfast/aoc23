5bac43e56f0626cb1abdcfb5a044abe6cd10868a
John Donnellan <john@codybartfast.com>
2023-11-30 22:07:20 +0000

    'Twas the night before Advent of Code

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.IO
+
+let inputFile = Path.Combine("../../../input/2023", "day01", "input.txt")
+let lines = File.ReadAllLines(inputFile)
+
+let parseLine line = line
+
+let items = lines |> Array.map parseLine
+
+printfn $"""{String.concat ", " items}"""
