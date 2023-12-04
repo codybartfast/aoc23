2d9cc8da18ef24ef32c7e60a189720080e2ca94d
John Donnellan <john@codybartfast.com>
2023-12-04 12:39:24 +0000

    Scratch-cade

--- Day04.fs
+++ Day04.fs
@@ -3,23 +3,23 @@ module Day04
 open System.Text.RegularExpressions
 
 let toCard line =
-    let [| _ ; winTxt ; heldTxt |] = Regex.Split(line, @"\s*[:\|]\s*")
+    let [| _ ; winTxt ; heldTxt |] = Regex.Split(line, @"\s*[:\|]\s+")
     let toNums (txt: string) =
         Regex.Split(txt, @"\s+") |> Array.map int |> List.ofArray
     (toNums winTxt, toNums heldTxt)
 
-let countMatches (winners, held) =
-    held |> List.filter (fun h -> List.contains h winners) |> List.length
-
-let score card =
-    card |> countMatches |> (function | 0 -> 0 | n -> 1 <<< (n - 1))
+let matchCounts lines =
+    let countMatches (winners, held) =
+        held |> List.filter (fun h -> List.contains h winners) |> List.length
+    lines |> List.map (toCard >> countMatches)
 
 let part1 getLines =
-    getLines "input" |> List.map toCard |> List.sumBy score
+    getLines "input"
+    |> matchCounts
+    |> List.sumBy (function 0 -> 0 | mc -> 1 <<< (mc - 1))
 
 let part2 getLines =
-    let matchCounts =
-        getLines "input" |> List.map (toCard >> countMatches) |> Array.ofList
+    let matchCounts = getLines "input" |> matchCounts |> Array.ofList
     let cardCounts = Array.init matchCounts.Length (fun _ -> 1)
 
     (cardCounts, matchCounts) ||> Array.iteri2 (fun i nCards nMatches ->
