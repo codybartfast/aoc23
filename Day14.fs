083671b80155afb85a89ffc4c7966238144e80ec
John Donnellan <john@codybartfast.com>
2023-12-14 14:11:23 +0000

    tilt!

--- Day14.fs
+++ Day14.fs
@@ -1,64 +1,57 @@
 module Day14
 
-type Platform = char [] []
+let rotateRight = Array.transpose >> Array.map Array.rev
 
-let rotateRight (platform: Platform) =
-    platform |> Array.transpose |> Array.map Array.rev
+let rotateLeft = Array.map Array.rev >> Array.transpose
 
-let rotateLeft (platform: Platform) =
-    platform |> Array.map Array.rev |> Array.transpose
-
-
-(*  The platform is orientated with North on the left so that stones roll
+(*  Orientate the platform with North on the 'left' so that stones roll
     within a row  *)
 let parseLines lines =
     let parseLine (line: string) = line.ToCharArray()
-    lines |> List.map parseLine |> Array.ofList |> rotateLeft
+    lines |> List.map parseLine |> Array.ofList
+    |> rotateLeft
 
-let tiltLeft (platform: Platform) =
+let tiltLeft platform =
     let rec findRest (row: char[]) i =
         let nxt = i - 1
-        if nxt >= 0 && row[nxt] = '.' then findRest row nxt else  i
+        if nxt >= 0 && row[nxt] = '.' then findRest row nxt else i
     let tiltRowLeft (row: char []) =
-        let row = Array.copy row
         row |> Array.iteri (fun i c ->
             if c = 'O' then
                 let dest = findRest row i
                 row[i] <- '.'
                 row[dest] <- 'O')
         row
-    platform |> Array.map tiltRowLeft
+    platform |> Array.map (Array.copy >> tiltRowLeft)
 
 let rec repeat func n value =
     if n = 0 then value else repeat func (n - 1) (func value)
 
-let cycle  = repeat (tiltLeft >> rotateRight) 4
+let cycle = repeat (tiltLeft >> rotateRight) 4
 
-let rec findDuplicate (nCycles: int) known (platform: Platform)  =
-    let key = platform
-    match Map.tryFind key known with
-    | Some n ->
-        (n, nCycles), platform
-    | _ ->
-        let known = known |> Map.add key nCycles
-        findDuplicate (nCycles + 1) known (cycle platform)
-
-let totalLoad (platform: Platform) =
-    platform
-    |> rotateLeft
-    |> Array.mapi (fun i row ->
+let totalLoad =
+    rotateLeft
+    >> Array.mapi (fun i row ->
         (i + 1) * (row |> Array.filter ((=) 'O') |> Array.length))
-    |> Array.sum
+    >> Array.sum
+
+let rec findDuplicate nCycles known platform  =
+    let key = (platform |> totalLoad) * (platform |> rotateLeft |> totalLoad)
+    match Map.tryFind key known with
+    | Some n ->  (n, nCycles), platform
+    | _ -> findDuplicate
+            (nCycles + 1)
+            (known |> Map.add key nCycles)
+            (cycle platform)
 
 let part1 getLines =
-    "input" |> getLines  |> parseLines
+    "input" |> getLines |> parseLines
     |> tiltLeft
     |> totalLoad
 
 let part2 getLines =
-    let (first, second), platform =
-        "input" |> getLines  |> parseLines
-        |> findDuplicate 0 (Map.empty<Platform, int>)
-    let remaining = (1_000_000_000 - second) % (second - first)
-    repeat cycle remaining platform
+    let platform = "input" |> getLines |> parseLines
+    let (fstCycle, dupCycle), dupPlat = platform |> findDuplicate 0 Map.empty
+    let remaining = (1_000_000_000 - dupCycle) % (dupCycle - fstCycle)
+    repeat cycle remaining dupPlat
     |> totalLoad
