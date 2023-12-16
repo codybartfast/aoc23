d13124b3bcb83dc518584bbbdb57e07620e959b5
John Donnellan <john@codybartfast.com>
2023-12-16 19:39:29 +0000

    Wait... is that Bruce Lee?

--- Day16.fs
+++ Day16.fs
@@ -1,8 +1,5 @@
 module Day16
 
-open System
-open System.Text.RegularExpressions
-
 type Cave = char [][]
 type Dir = N | E | S | W
 type Beam = (int * int) * Dir
@@ -10,19 +7,6 @@ type Energized = (Dir list)[][]
 
 let readCave (lines: string list) =
     lines |> List.map _.ToCharArray() |> Array.ofList
-
-let display (energized: Energized) (((x, y), dir): Beam) (otherBeams: (Beam option) list) =
-    printfn ""
-    let lensArray =
-        energized
-        |> Array.map(fun row ->
-                row
-                |> Array.map (fun dirList -> dirList.Length.ToString()))
-    lensArray[y][x] <- dir.ToString()
-    lensArray
-    |> Array.iter(fun row ->
-        row |> String.concat "" |> _.Replace("0", ".") |> printfn "%s")
-    printfn ""
 
 let nextBeam (cave: Cave) (((x,y), dir): Beam) =
     let x', y' =
@@ -39,14 +23,10 @@ let nextBeam (cave: Cave) (((x,y), dir): Beam) =
 let nextDirections (tile: char) (curr: Dir)=
     match tile, curr with
     | '.', _ -> [curr]
-    | '|', N
-    | '|', S -> [curr]
-    | '|', W
-    | '|', E -> [N; S]
-    | '-', W
-    | '-', E -> [curr]
-    | '-', N
-    | '-', S -> [W; E]
+    | '|', N | '|', S -> [curr]
+    | '|', W | '|', E -> [N; S]
+    | '-', W | '-', E -> [curr]
+    | '-', N | '-', S -> [W; E]
     | '/', N -> [E]
     | '/', E -> [N]
     | '/', W -> [S]
@@ -57,41 +37,38 @@ let nextDirections (tile: char) (curr: Dir)=
     | '\\', W -> [N]
 
 let shine (cave: Cave) (mbeam: Beam option) (energized: Energized) : (Beam option) list =
-    let rec shine (mbeam: Beam option) (newBeams : (Beam option) list) : (Beam option) list =
-        match mbeam with
-        | None -> newBeams
+    let rec shine mayBeam unfollowedBeams =
+        match mayBeam with
+        | None -> unfollowedBeams
         | Some ((x, y), dir) ->
         let prevDirs = energized[y][x]
-        if energized[y][x] |> List.contains dir then
-            newBeams
+        if prevDirs |> List.contains dir then
+            unfollowedBeams
         else
             energized[y][x] <- (dir::prevDirs)
             let (nextDir::newDirs) = nextDirections (cave[y][x]) dir
-            let newBeams' = (newDirs |> List.map (fun nd -> nextBeam cave ((x, y), nd))) @ newBeams
-            shine (nextBeam cave ((x, y), nextDir)) newBeams'
+            let unfollowedBeams' =
+                (newDirs |> List.map (fun dr -> nextBeam cave ((x, y), dr)))
+                    @ unfollowedBeams
+            shine (nextBeam cave ((x, y), nextDir)) unfollowedBeams'
     shine mbeam []
 
-let rec shineAll (cave: Cave) (beams: Beam option list) (energized: Energized) =
-    match beams with
+let rec shineAll (cave: Cave) (unfollowedBeams: Beam option list) (energized: Energized) =
+    match unfollowedBeams with
     | [] -> energized
-    | beam::beams' ->
+    | beam::unfollowedBeams ->
         let newBeams = shine cave beam energized
-        shineAll cave (newBeams @ beams') energized
+        shineAll cave (newBeams @ unfollowedBeams) energized
 
-let energizedCount (energized: Energized) =
-    energized
-    |> Array.collect id
-    |> Array.filter ((<>) [])
-    |> Array.length
+let energizedCount  =
+    Array.collect id >> Array.filter ((<>) [])  >> Array.length
 
 let newEnergized (cave: Cave) =
     Array.init cave.Length (fun _ -> Array.create cave[0].Length [])
 
 let starts (cave: Cave) =
-    let maxX = cave[0].Length - 1
-    let maxY = cave.Length - 1
-    let ys = [0 .. maxY]
-    let xs = [0 .. maxX]
+    let maxX, maxY = cave[0].Length - 1, cave.Length - 1
+    let ys, xs = [0 .. maxY], [0 .. maxX]
     let fromLeft = ys |> List.map (fun y -> ((0, y), E))
     let fromRight = ys |> List.map (fun y -> ((maxX, y), W))
     let fromTop = xs |> List.map (fun x -> ((x, 0), S))
@@ -109,6 +86,5 @@ let part2 (getLines: string -> string list) =
 
     starts cave
     |> List.map (fun start ->
-        (shineAll cave [start] (newEnergized cave))
-        |> energizedCount)
+        energizedCount <| shineAll cave [start] (newEnergized cave))
     |> List.max
